031d312927d8b5078587a2e4f39b2e7ec53adc4a
Asiful Haque <asiful35-2961@diu.edu.bd>
2024-11-07 14:11:16 +0600

    A

--- src/meaning2/meaning2.service.ts
+++ src/meaning2/meaning2.service.ts
@@ -4,5 +4,5 @@ import { Injectable } from '@nestjs/common';
 export class meaning2Service {
   async getMean(language: string, word: string): Promise<string> {
     return 'Hello asif';
-  }
+  }     
 }
